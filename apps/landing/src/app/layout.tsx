1357c94fa852e283c4edd0e2ca1ca380f3fe6fd9
Ounza B. <bravinshumwe.coding@gmail.com>
2026-02-10 01:23:17 +0300

    first commit

---
+++ apps/landing/src/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import Fathom from "@/component/Fathom";
+
+export const metadata: Metadata = {
+  title: "Truehost Cloud Issues",
+  description:
+    "This is a self-hosted issue tracker for your projects or help desk.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`antialiased`}>
+        <Fathom />
+        {children}
+      </body>
+    </html>
+  );
+}
